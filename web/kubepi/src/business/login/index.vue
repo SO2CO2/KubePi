40272a983751780fa40693ba96cf58d822d87a3c
shaofeng <shaofeng_sh@126.com>
2023-10-12 11:24:40 +0800

    调整登录框尺寸

--- web/kubepi/src/business/login/index.vue
+++ web/kubepi/src/business/login/index.vue
@@ -229,8 +229,8 @@ export default {
   }
 
   .login-container {
-    min-width: 640px;
-    width: 960px;
+    min-width: 320px;
+    width: 320px;
     height: 480px;
     background-color: #FFFFFF;
     box-shadow: 0 5px 5px -3px rgba(0, 0, 0, .2), 0 8px 10px 1px rgba(0, 0, 0, .14), 0 3px 14px 2px rgba(0, 0, 0, .12);
